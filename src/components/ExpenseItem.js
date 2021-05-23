0f1b7398ed259bd1ded08c57b5b127ac58658c91
Dilara Erhalim <dilaraeceozturk@gmail.com>
2021-05-23 22:47:28 +0300

    splitting components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenteItem(props) {
+  const month = props.date.toLocaleString("tr-TR", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("tr-TR", { day: "2-digit" });
+
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
