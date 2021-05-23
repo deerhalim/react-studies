b33e25aa62e898fa95392410609c944189576e21
Dilara Erhalim <dilaraeceozturk@gmail.com>
2021-05-23 22:11:17 +0300

    component initialize with props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import ExpenteItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 35.2,
+      date: new Date(2020, 8, 14),
+    },
+    {
+      id: "e3",
+      title: "TV",
+      amount: 51.89,
+      date: new Date(2020, 9, 14),
+    },
+    {
+      id: "e4",
+      title: "Toilet Paper",
+      amount: 64.12,
+      date: new Date(2020, 10, 14),
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +47,27 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <ExpenteItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenteItem>
+       <ExpenteItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenteItem>
+       <ExpenteItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenteItem>
+       <ExpenteItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenteItem>
     </div>
   );
 }
